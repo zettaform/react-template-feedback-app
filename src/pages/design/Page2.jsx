8dbf0c5b0e6c936dc735864d2912cfc1e82686b9
Webel <webel@Webels-MacBook-Air.local>
2025-08-22 18:32:07 +0530

    Prune to minimal FastAPI backend only

--- src/pages/design/Page2.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Customers from '../ecommerce/Customers';
-
-export default function DesignPage2() {
-  // Reuse the Customers page to mirror the design at /ecommerce/customers
-  return <Customers />;
-}
